6147485be152d722437c4a0ce3c757bc989f9c29
Ibuki <P.Kellermachine@gmail.com>
2016-03-25 06:10:42 +0000

    create top_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root  'home#top'
+  # get '/new_user' => '#new_user'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
