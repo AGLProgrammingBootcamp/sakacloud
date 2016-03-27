f9a72a8ebb7c1b71133ba7ab75f06949a2b8a5a3
daikinagahama <nagahama.d.aa@m.titech.ac.jp>
2016-03-27 05:17:34 +0000

    heroku

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Sakacloud
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += Dir["#{config.root}/lib"]
     # For Heroku
-    config.assets.initialize_on_precompile = false
+#    config.assets.initialize_on_precompile = false
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
