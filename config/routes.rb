2903813724bbb3e5ff46c99be4ad2a3b42d2b0de
daikinagahama <nagahama.d.aa@m.titech.ac.jp>
2016-03-27 00:35:39 +0000

    create graph

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :sakes
   resources :users
   get 'user_list' => 'user#index'
+  get 'graph' => 'graph#create_graph'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
